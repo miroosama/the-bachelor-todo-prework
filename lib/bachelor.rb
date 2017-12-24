7bc8da32ff0c1f33f5d4c887628b89befeb1c037
osama miro <o.miro650@gmail.com>
2017-12-24 05:31:34 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -53,7 +53,7 @@ def get_occupation(data, hometown)
   data.each do |hash_season, contestants_array|
     contestants_array.each do |contestant|
       if contestant["hometown"] == hometown
-        person = contestant["occupation"
+        person = contestant["occupation"]
         return person
       end
     end
